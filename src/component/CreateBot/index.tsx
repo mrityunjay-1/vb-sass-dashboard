1235f9047a3d28c88493efe162fca6761cf6c777
Mrityujay Kumar <kumarmrityunjay7034@gmail.com>
2023-05-10 01:20:22 +0530

    bot creation

--- src/component/CreateBot/index.tsx
+++ src/component/CreateBot/index.tsx
@@ -1,29 +1,74 @@
-import { NavLink, useParams } from "react-router-dom";
+// import { NavLink, useParams } from "react-router-dom";
 import Header from "../Header/Header";
-import { useEffect } from "react";
-import { useAuth } from "../../context/AuthContext";
+import { useState } from "react";
+// import { useAuth } from "../../context/AuthContext";
+import fetchData from "../../data/fetchData";
+import { message } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const CreateBot = () => {
 
-    const params = useParams();
-    const auth: any = useAuth();
+    // const params = useParams();
+    // const auth: any = useAuth();
 
-    console.log("Params: ", params);
+    const navigate = useNavigate();
 
-    useEffect(() => {
-        auth.setBotInContext(params.botId);
-    }, []);
+    // console.log("Params: ", params);
+
+    // useEffect(() => {
+    //     auth.setBotInContext(params.botId);
+    // }, []);
+
+    const [botName, setBotName] = useState("");
+
+    const createBot = async () => {
+        try {
+
+            const res = await fetchData({
+                url: "/createBot",
+                method: "POST",
+                data: {
+                    botName,
+                },
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+
+            console.log("Response : ", res);
+
+            if (res) {
+                message.success("Bot Succesfully Created");
+                navigate("/");
+            }
+
+        } catch (err) {
+            console.log("Error: ", err);
+        }
+    }
 
     return (
         <>
 
             <Header Title="Create Bot" />
 
-            <h1>Let's Create Bot</h1>
+            <div>
+                <h1>Let's Create Bot</h1>
+
+                <input
+                    type="text"
+                    value={botName}
+                    onChange={(e) => {
+                        setBotName(e.target.value);
+                    }}
+                />
+
+                <button onClick={createBot}>Create</button>
+            </div>
 
             {/* <Navigate to="/channels"></Navigate> */}
 
-            <NavLink to="channels">Channel</NavLink>
+            {/* <NavLink to="channels">Channel</NavLink> */}
         </>
     );
 }
--- src/component/WelcomePage/index.tsx
+++ src/component/WelcomePage/index.tsx
@@ -1,14 +1,41 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Header from "../Header/Header";
 import { useAuth } from "../../context/AuthContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import fetchData from "../../data/fetchData";
 
 const WelcomePage = () => {
 
     const auth: any = useAuth();
 
+    const [bots, setBots] = useState([]);
+
+    const navigate = useNavigate();
+
     useEffect(() => {
+
         auth.checkIfLoggedIn();
+
+        (
+            async () => {
+                try {
+
+                    const res = await fetchData({
+                        url: "/getAllBots"
+                    });
+
+                    console.log("response: ", res);
+
+                    if (res) {
+                        setBots(res.reverse());
+                    }
+
+                } catch (err) {
+                    console.log("Error : ", err);
+                }
+            }
+        )();
+
     }, []);
 
     return (
@@ -20,6 +47,18 @@ const WelcomePage = () => {
             {/* <h1>Welcome</h1> */}
 
             <NavLink to={"/create-bot"}>Create Bot</NavLink>
+
+            {
+                bots?.map((bot: any) => {
+                    return (
+                        <div>
+                            {bot.botName}
+
+                            <button onClick={() => navigate(`manage-bot/${bot.botId}`)} >More</button>
+                        </div>
+                    );
+                })
+            }
 
         </>
     );
